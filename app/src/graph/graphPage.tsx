3a8b74ef22762a3962c4b1032886d164538f9330
Anton Vitus Sohl Andersen <anton.sohl@gmail.com>
2025-03-26 15:22:55 +0100

    Dropdown added and two more graphs

--- app/src/graph/graphPage.tsx
+++ app/src/graph/graphPage.tsx
@@ -1,18 +1,51 @@
-import Graph from './graph';
+import { useState } from 'react';
+import { Dropdown } from 'flowbite-react';
+import CircleGraph from './circleGraph';
+import BreadthfirstGraph from './breadthfirstGraph';
+import RandomGraph from './randomGraph';
 import EditorPage from '~/src/editor/editorPage';
 import LogTracer from '~/src/tracer/tracer.tsx';
-import ResizableBox from '~/src/resizable/resizable.tsx'
+import ResizableBox from '~/src/resizable/resizable.tsx';
 
 export default function GraphPage() {
+    const [selectedGraph, setSelectedGraph] = useState('Graph 1');
+
+    function renderGraph() {
+        switch (selectedGraph) {
+            case 'Graph 1':
+                return <CircleGraph />;
+            case 'Graph 2':
+                return <BreadthfirstGraph />;
+            case 'Graph 3':
+                return <RandomGraph />;
+            default:
+                return null;
+        }
+    }
+
     return (
         <div className="flex h-screen -mt-20 pt-20">
             <div className="flex-1 flex flex-col border-r border-gray-200">
+                <div className="p-4">
+                    <Dropdown label={selectedGraph}>
+                        <Dropdown.Item onClick={() => setSelectedGraph('Graph 1')}>
+                            Graph 1
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => setSelectedGraph('Graph 2')}>
+                            Graph 2
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => setSelectedGraph('Graph 3')}>
+                            Graph 3
+                        </Dropdown.Item>
+                    </Dropdown>
+                </div>
+
                 <div className="flex-1 basis-2/3">
                     <ResizableBox>
-                        <Graph />
+                        {renderGraph()}
                     </ResizableBox>
-                    
                 </div>
+
                 <div className="flex-1 basis-1/3 border-t border-gray-200 overflow-y-auto">
                     <LogTracer />
                 </div>
@@ -22,7 +55,6 @@ export default function GraphPage() {
                 <ResizableBox>
                     <EditorPage />
                 </ResizableBox>
-                
             </div>
         </div>
     );
